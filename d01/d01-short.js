8923a08685a80598419c03dc37c23ecd5c22d1a2
Mark Van houtert <mvan-hou@f1r3s13.codam.nl>
2021-12-01 21:59:54 +0100

    shorter still

--- d01/d01-short.js
+++ d01/d01-short.js
@@ -1,5 +1,5 @@
 //d01
-console.log('p1 '+(require('fs').readFileSync('1')+"").split('\n').filter((a,b,c)=>a>c[b-1]/1).length)
+console.log((require('fs').readFileSync("1")+"").split`\n`.filter((a,b,c)=>a>+c[b-1]).length)
 
 //d01-2
-console.log('p2 '+(require('fs').readFileSync('1')+"").split('\n').filter((a,b,c)=>a>c[b-3]/1).length)
+console.log((require('fs').readFileSync('1')+"").split`\n`.filter((a,b,c)=>a>+c[b-3]).length)
