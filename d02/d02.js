ab44ebe1524cdc8a16142d381835c58cbf72b3eb
mvhou <mark.vanhoutert@gmail.com>
2021-12-02 07:00:57 +0100

    d02 quick

---
+++ d02/d02.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+let input = fs.readFileSync("2").toString().split('\n').map(x => [x.split(' ')[0], parseInt(x.split(' ')[1])])
+
+let subPos = {
+  'horizontal': 0,
+  'depth': 0,
+  'aim': 0
+}
+
+input.forEach((x) => {
+  if (x[0] == 'forward') {
+    subPos.horizontal += x[1];
+    subPos.depth += subPos.aim * x[1];
+  }
+  else if (x[0] == 'up') {
+    // subPos.depth -= x[1];
+    subPos.aim -= x[1];
+  }
+  else if (x[0] == 'down') {
+    // subPos.depth += x[1];
+    subPos.aim += x[1];
+  }
+})
+
+console.log(subPos.horizontal * subPos.depth)
