a904ff18755baad898d76d449f918cf0c8e78256
Mark van Hou <mvhou@Marks-MacBook-Air.local>
2021-12-02 01:31:03 +0100

    simplified

--- d01/d01.hs
+++ d01/d01.hs
@@ -1,13 +1,13 @@
 module D01 where
 import Data.List.Split
 
-d01 :: Int -> IO ()
-d01 part = do
-    input <- readFile "1"
-    print $ getAnswer (map (read::String->Int) (splitOn "\n" input)) part
 
-part1 = 1::Int
-part2 = 3::Int
+
+d01 :: IO ()
+d01 = do
+    input <- readFile "1"
+    print $ getAnswer (map (read::String->Int) (splitOn "\n" input)) 1
+    print $ getAnswer (map (read::String->Int) (splitOn "\n" input)) 3
 
 getAnswer :: [Int] -> Int -> Int
 getAnswer xs part = length . filter (\x -> fst x < snd x) . zip xs $ drop part xs
