d51513d09d1e425518bed436162040a623e431e1
Mark Van houtert <mvan-hou@f0r4s21.codam.nl>
2021-12-03 14:54:08 +0100

    comments

--- random/d02-short.py
+++ random/d02-short.py
@@ -1,3 +1,4 @@
-p=d=0
-for n in open("2"):x,y=n.split();y=int(y);p+=(x[0]=='f')*y;d+=((x<'f')-(x>'m'))*y
-print(p*d)
+# lgutter code
+# p=d=0
+# for n in open("2"):x,y=n.split();y=int(y);p+=(x[0]=='f')*y;d+=((x<'f')-(x>'m'))*y
+# print(p*d)
--- random/d02a-short.py
+++ random/d02a-short.py
@@ -1,5 +1,6 @@
-p=d=a=0
-for l in open("2"):
-	c,v=l.split();v=int(v);a+=((c<'f')-(c>'g'))*v
-	if c[0]=='f':p+=v;d+=v*a
-print(p*d)
+# lgutter code
+# p=d=a=0
+# for l in open("2"):
+# 	c,v=l.split();v=int(v);a+=((c<'f')-(c>'g'))*v
+# 	if c[0]=='f':p+=v;d+=v*a
+# print(p*d)
