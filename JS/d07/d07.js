2895098c45654e4c3d5726c273dd94d4e48f66b6
mvhou <mark.vanhoutert@gmail.com>
2021-12-08 00:38:00 +0100

    faster and benchmarking

--- JS/d07/d07.js
+++ JS/d07/d07.js
@@ -1,13 +1,19 @@
+import {performance, PerformanceObserver} from 'perf_hooks';
+
 import reader from '../readFile.js'
 import mark from '../markModule.js'
 
+const start = performance.now();
 const input = reader.getLinesBy("d07", ',').map(x => +x);
 
 const median = mark.median(input);
 const mean = Math.round(mark.mean(input, false) - 0.25); //this is bs, how make better?
 
+const tri = (n) => (n * (n + 1)) / 2;
+
 const part1 = (arr) => arr.reduce((acc, x) => acc + Math.abs(median - x), 0)
-const part2 = (arr) => arr.reduce((acc, x) => acc + mark.sum(mark.newArray(1, Math.abs(mean - x))) ,0)
+const part2 = (arr) => arr.reduce((acc, x) => acc + tri(Math.abs(mean - x)) ,0)
 
 console.log("part 1 =", part1(input));
 console.log("part 2 =", part2(input));
+console.log(performance.now() - start);
